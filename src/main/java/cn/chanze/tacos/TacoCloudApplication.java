139a9359edbaf2d55c2c779a67b70d0821c0eee0
ChanzeZheng <40560646+ChanzeZheng@users.noreply.github.com>
2020-04-18 12:25:46 +0800

    1.2 对Spring Initializr提供的代码进行解析注释

--- src/main/java/cn/chanze/tacos/TacoCloudApplication.java
+++ src/main/java/cn/chanze/tacos/TacoCloudApplication.java
@@ -3,11 +3,11 @@ package cn.chanze.tacos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication                  //Spring Boot 应用
 public class TacoCloudApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TacoCloudApplication.class, args);
+        SpringApplication.run(TacoCloudApplication.class, args);            //运行应用
     }
 
 }
--- src/test/java/cn/chanze/tacos/TacoCloudApplicationTests.java
+++ src/test/java/cn/chanze/tacos/TacoCloudApplicationTests.java
@@ -1,12 +1,15 @@
 package cn.chanze.tacos;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)                    //使用Spring的运行器
+@SpringBootTest                                 //Spring Boot测试
 class TacoCloudApplicationTests {
 
-    @Test
+    @Test                                       //测试方法
     void contextLoads() {
     }
 
